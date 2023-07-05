a5061ab5d0e799db276cae6ac4dcbe02d18d308f
Gt-gih <girmaytekle84@gmail.com>
2023-07-05 22:44:58 +0300

    tasks added

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" "
+# takes in a URL and displays all HTTP methods the server will accept.
+curl -Is "$1" | grep "Allow:" | cut -d ":" -f 2 | cut -c 2- | rev | cut -c 2- | rev
